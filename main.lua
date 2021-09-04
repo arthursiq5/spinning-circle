87933f21d5c8c7b7edb2ad5457d5e660630a5912
arthursiq5 <arthur.siq5@gmail.com>
2021-09-03 21:03:08 -0300

    Criado sistema para girar a imagem

--- main.lua
+++ main.lua
@@ -4,3 +4,23 @@ angulo = 0
 tamanho = 1
 origem = 125
 
+function love.load()
+    imagem = love.graphics.newImage('img/circle.png')
+    love.mouse.setVisible(false)
+end
+
+function love.update(dt)
+    if love.keyboard.isDown('left') then
+        posX = posX - 100 * dt
+        angulo = angulo - dt * 1.5
+    end
+    if love.keyboard.isDown('right') then
+        posX = posX + 100 * dt
+        angulo = angulo + dt * 1.5
+    end
+end
+
+function love.draw()
+    rot = angulo
+    love.graphics.draw(imagem, posX, posY, rot, tam, tam, origem, origem)
+end
